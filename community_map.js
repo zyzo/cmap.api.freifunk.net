416851fc8b5f9860e27529ede062ca5c6de68c4f
zyzo <danghaian168@gmail.com>
2015-07-01 09:53:14 +0200

    apply changes from fossasia/cmap.api.fossasia.net branch add-popup-info from a31d702 to 4461b2b
        https://github.com/fossasia/cmap.api.fossasia.net/commit/a31d70279e76099aacf48ac548575e7ad5b376d8
        https://github.com/fossasia/cmap.api.fossasia.net/commit/2a2dd2b4ef83c46c5019a88f6444db2f6db9a9da
        https://github.com/fossasia/cmap.api.fossasia.net/commit/2a2dd2b4ef83c46c5019a88f6444db2f6db9a9da
        https://github.com/fossasia/cmap.api.fossasia.net/commit/1fdb9d7032af6ea787f42f6d801d7a645f31a948
        https://github.com/fossasia/cmap.api.fossasia.net/commit/4461b2b3d44d2375ccad24efe0fe51d54f04249f

--- community_map.js
+++ community_map.js
@@ -1,6 +1,5 @@
 var FFCommunityMapWidget = function(settings, map_options, link) {
-  
-  var renderPopup = function (props) {
+  var renderPopup = function (props, configs) {
     //console.log(props);
     //clean up values before rendering
     if (props.url && !props.url.match(/^http([s]?):\/\/.*/)) { 
@@ -106,7 +105,7 @@ var FFCommunityMapWidget = function(settings, map_options, link) {
       });
     }
     
-    
+    props.embedTimelineUrl = configs.embedTimelineUrl;
     //render html and return
     return widget.communityTemplate(props);
   };
@@ -175,11 +174,11 @@ attribution: '<a href="https://www.mapbox.com/about/maps/" target="_blank">&copy
       "OSM": osmlayer
     }).addTo(widget.map);
   }
-  
+  $.getJSON('config.json', function(configs) {
   $.getJSON(options.geoJSONUrl, function(geojson) {
     var geoJsonLayer = L.geoJson(geojson, {
       onEachFeature: function(feature, layer) {
-        layer.bindPopup(options.getPopupHTML(feature.properties), { minWidth: 210 });
+        layer.bindPopup(options.getPopupHTML(feature.properties, configs), { minWidth: 210 });
       },
       filter: function(feature, layer) {
         if (feature.geometry.coordinates[0] && feature.geometry.coordinates[1]) {
@@ -227,6 +226,46 @@ attribution: '<a href="https://www.mapbox.com/about/maps/" target="_blank">&copy
   widget.communityTemplate = _.template(
     $( "script.template#community-popup" ).html()
   );
-  
+
+   widget.map.on('popupopen', function(e){
+    var url = configs.feedUrl
+      + '?limit=3&source='
+      + e.popup._contentNode.getElementsByClassName('community-popup')[0].getAttribute('data-id');
+    console.log(url);
+    $.ajax({
+      url: url,
+      error: function(err) {
+        console.log(err);
+      },
+      success: function(data) {
+        $data = $(data)
+        items = $data.find('item');
+        if (items.length > 0) {
+          console.log('There are some items');
+          var rssfeed = $(e.popup._container).find('.community-popup').append('<div class="rssfeed">').find('.rssfeed');
+          rssfeed.append('<label>Recent posts</label>');
+          items.each(function(k, item) {
+            var blogLink = rssfeed.append('<a class="bloglink" target="_blank">' + $(item).find('title').text() + '</a>').find('a').last();
+            blogLink.attr('href', $(item).find('link').text());
+          });
+        }
+      },
+      timeout: 20000
+    });
+  })
+ });
   return widget;
 }
+
+var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
+var eventer = window[eventMethod];
+var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
+
+// Listen to message from child window
+eventer(messageEvent,function(e) {
+  if (e.data == ("embed-timeline-loaded")) {
+    var key = e.message ? "message" : "data";
+    var data = e[key];
+    $('.events').removeClass('hidden');
+  }
+},false);
